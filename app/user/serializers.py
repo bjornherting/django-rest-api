78b1baeb97602dc203ae8d311e23a1706c1cfd1c
Bjørn Kuvaas Herting <bjorn.herting@apparat.no>
2023-12-28 14:08:51 +0100

    add api endpoint for creating user

---
+++ app/user/serializers.py
@@ -0,0 +1,29 @@
+"""
+Serializer for the user api view
+"""
+
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the user model"""
+
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'email',
+            'password',
+            'name'
+        ]
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'min_length': 5
+            }
+        }
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password"""
+        return get_user_model().objects.create_user(**validated_data)
